ae5b1876b2a018292324bb95d3f52baac6ccf36d
zhangmeixi05-rgb <zhangmeixi05-rgb@users.noreply.github.com>
2026-01-16 13:39:12 +0800

    Update environment ID and add cloudfunctions folder

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ App({
     wx.setStorageSync('logs', logs)
 
       wx.cloud.init({
-        env: 'cloud1-3gr6n5xu22472ec5', // 你的环境ID
+        env: 'cloud1-8gkoymjia23bac63', // 你的环境ID
         traceUser: true, // 是否要在控制台打印用户身份信息，开发环境建议开启
       })
    
